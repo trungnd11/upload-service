ba0f97caa8c0d5241111b088c42c19ac80d20f56
trungnd <trung.dn9500@gmail.com>
2025-05-01 19:45:14 +0700

    feat: add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Upload Service API")
+    .setDescription("REST endpoints for upload/img management")
+    .setVersion("1.0")
+    .addTag("upload")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {});
+  SwaggerModule.setup("docs", app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
